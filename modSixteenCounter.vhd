e41fb6143240f4c844a0f60897ceac49b7d87990
falla055 <145510014+falla055@users.noreply.github.com>
2023-11-04 20:01:04 -0400

    update counter that actually works now

--- modSixteenCounter.vhd
+++ modSixteenCounter.vhd
@@ -13,7 +13,7 @@ entity modSixteenCounter is
 architecture rtl of modSixteenCounter is
 	signal int_q: std_logic_vector(3 downto 0);
 	signal int_qBar: std_logic_vector(3 downto 0);
-	signal int_inBetween: std_logic_vector(3 downto 0);
+	signal int_d: std_logic_vector(3 downto 0);
 	signal int_enable: std_logic;
 	signal int_set: std_logic;
 	signal int_load: std_logic;
@@ -39,7 +39,7 @@ architecture rtl of modSixteenCounter is
 		i_clear => i_reset,
 		i_set => int_set,
 		i_clock => i_clock,
-		i_d => int_inBetween(0),
+		i_d => int_d(0),
 		o_q => int_q(0),
 		o_qBar => int_qBar(0)
 	);
@@ -50,7 +50,7 @@ architecture rtl of modSixteenCounter is
 		i_clear => i_reset,
 		i_set => int_set,
 		i_clock => i_clock,
-		i_d => int_inBetween(1),
+		i_d => int_d(1),
 		o_q => int_q(1),
 		o_qBar => int_qBar(1)
 	);
@@ -61,7 +61,7 @@ architecture rtl of modSixteenCounter is
 		i_clear => i_reset,
 		i_set => int_set,
 		i_clock => i_clock,
-		i_d => int_inBetween(2),
+		i_d => int_d(2),
 		o_q => int_q(2),
 		o_qBar => int_qBar(2)
 	);
@@ -72,18 +72,20 @@ architecture rtl of modSixteenCounter is
 		i_clear => i_reset,
 		i_set => int_set,
 		i_clock => i_clock,
-		i_d => int_inBetween(3),
+		i_d => int_d(3),
 		o_q => int_q(3),
 		o_qBar => int_qBar(3)
 	);
 	
     	     int_set <= '0';
     	  int_enable <= i_count;
-    int_inBetween(0) <= int_qBar(0);
-    int_inBetween(1) <= int_q(0) XOR int_q(1);
-    int_inBetween(2) <= (int_q(2) and int_qBar(0)) or (int_q(2) and int_qBar(1)) or (int_qBar(3) and int_q(1) and int_q(0));
-    int_inBetween(3) <= (int_q(3) and int_qBar(1)) or (int_q(3) and int_qBar(0)) or (int_q(3) and int_qBar(2)) or (int_qBar(3) and int_q(2) and int_q(1) and int_q(0));
-		   o <= int_q;
+    int_d(0) <= (int_qBar(1)and int_qBar(0)) or (int_q(1) and int_qBar(0));
+    int_d(1) <= int_q(0) XOR int_q(1);
+    int_d(2) <= (int_q(2) and int_qBar(1)) or (int_qBar(2) and int_q(1) and int_q(0))
+						or (int_q(2) and int_q(1) and int_qBar(0));
+    int_d(3) <= (int_q(3) and int_qBar(1)) or (int_q(3) and int_qBar(2)) or
+						(int_q(3) and int_q(1) and int_qBar(0)) or (int_qBar(3) and int_q(2) and int_q(1) and int_q(0));
+	 o <= int_q;
     
 
 end rtl;
